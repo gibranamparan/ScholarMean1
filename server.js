96077d856eab068e6e4b1fff5d244e245779f21c
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-02-13 07:39:09 -0700

    Heroku config deply
    Se agregaron instrucciones en package.json para ejecutar la aplicacion
    en el server.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Point static path to dist
+console.log('__dirname'+__dirname);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 //Instace server
