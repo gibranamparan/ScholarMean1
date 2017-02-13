ef77c0d85678b877a8f779fd79ed8cdfbceb863b
Gibran Amparan <GibranAmparanD@hotmail.com>
2017-02-13 08:11:37 -0700

    Correccino de errores de importacion
    y una prueba con socket io client

--- src/app/alumno/alumno.service.ts
+++ src/app/alumno/alumno.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http} from '@angular/http'
 import 'rxjs/add/operator/map'
 import { GlobalParamsService } from '../global-params.service';
-import { Alumno } from './Alumno';
+import { Alumno } from './alumno';
 
 export enum AlumnoStatus{
 	EnRegistro=1, Preinscrito, Inscrito	
